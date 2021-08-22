0f0bff463e0009b5d0214c4b5115db606fabd0fb
psnavya <ayvans90@yahoo.in>
2021-08-23 03:23:23 +0530

    config files, test folder, test data skeleton added

---
+++ src/main/java/entities/EmployeeDetails.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class EmployeeDetails{
+    public String employeeName;
+    public LeaveDetails leaveDetails;
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public LeaveDetails getLeaveDetails() {
+        return leaveDetails;
+    }
+
+    public void setLeaveDetails(LeaveDetails leaveDetails) {
+        this.leaveDetails = leaveDetails;
+    }
+}
---
+++ src/main/java/entities/LeaveDetails.java
@@ -0,0 +1,41 @@
+package entities;
+
+public class LeaveDetails{
+    public String leaveType;
+    public int entitlement;
+    public String fromDate;
+    public String toDate;
+
+    public String getLeaveType() {
+        return leaveType;
+    }
+
+    public void setLeaveType(String leaveType) {
+        this.leaveType = leaveType;
+    }
+
+    public int getEntitlement() {
+        return entitlement;
+    }
+
+    public void setEntitlement(int entitlement) {
+        this.entitlement = entitlement;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+}
